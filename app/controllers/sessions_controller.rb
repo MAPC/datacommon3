aae0afe355bb6c1b76c1370a4e7184d0ba438c79
Matt Cloyd <cloydster@gmail.com>
2014-09-02 21:46:49 -0400

    Signout, header menus working, flash renders. User model still kept out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,11 @@ class SessionsController < ApplicationController
       flash[:success] = "Welcome to the DataCommon!"
       redirect_to user
     else
-      flash[:danger] = <<-EOS
+      flash.now[:danger] = <<-EOS
         We don't know that username/password combination.
         You may have mistyped something, or have CAPS LOCK on.
       EOS
-
-      redirect_to signin_path
+      render 'new'
     end
   end
 
