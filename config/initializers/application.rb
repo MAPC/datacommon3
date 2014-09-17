4b48512b559988716d2f7f775b85f792f5223609
Matt Cloyd <cloydster@gmail.com>
2014-09-16 23:33:08 -0400

    Forgot to remove ActiveRecord from the other code. Referring to active_resource in initializers when it wasn't included as a gem anymore locked the initializers from loading.

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,6 +1,2 @@
-require 'active_resource'
-require "#{Rails.root}/app/lib/active_resource/redirection"
-require "#{Rails.root}/app/lib/active_resource/paginated_collection"
-
 require 'active_record'
 require 'active_record/add_reset_pk_sequence_to_base'
