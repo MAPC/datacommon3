ba5668d90072ba8c88e7bb9d152afcd908ce3732
Matt Cloyd <cloydster@gmail.com>
2014-12-11 13:23:16 -0500

    Add social icons to footer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0.1'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( facebook_50.png twitter_50.png flickr_50.png )
