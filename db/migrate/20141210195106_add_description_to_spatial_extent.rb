f7e9fa19cffd5a136772f596b6a95a0f2d664a15
Matt Cloyd <cloydster@gmail.com>
2015-01-19 17:50:26 -0500

    Remove migrations, because they're causing 'migration still to be run' errors

--- db/migrate/20141210195106_add_description_to_spatial_extent.rb
+++
@@ -1,7 +0,0 @@
-ActiveRecord::Base.establish_connection :geographic
-
-class AddDescriptionToSpatialExtent < ActiveRecord::Migration
-  def change
-    add_column :'_geo_extents', :desc, :text
-  end
-end
