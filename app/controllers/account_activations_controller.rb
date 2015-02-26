ce8945d0510cf0f17565d0f9fc0b441887a4c6f1
Matt Cloyd <cloydster@gmail.com>
2015-02-26 11:31:43 -0500

    Needed to reload user to get correct value -- it was activating properly the whole time after all.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,8 +1,11 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
+    puts "email: #{params[:email]}"
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attributes(is_active: true, activated_at: Time.zone.now)
+      puts "user activatable"
+      user.update_attribute(:is_active,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
       sign_in user
       flash[:success] = "Account activated! Welcome!"
       redirect_to user
--- spec/features/user_activates_inactive_account_spec.rb
+++ spec/features/user_activates_inactive_account_spec.rb
@@ -5,19 +5,19 @@ feature 'User activates inactive account' do
   scenario 'with a valid token' do
     user = create(:new_user)
     visit edit_account_activation_path(user.activation_token, email: user.email)
-    expect(user.activated?).to be_true
+    expect(user.reload.activated?).to be_true
   end
 
   scenario 'with the wrong token' do
     user = create(:new_user)
     visit edit_account_activation_path('FHQWGADS', email: user.email)
-    expect(user.activated?).to be_false
+    expect(user.reload.activated?).to be_false
   end
 
   scenario 'with the wrong email' do
     user = create(:new_user)
     visit edit_account_activation_path(user.activation_token, email: 'le@wrong.eml')
-    expect(user.activated?).to be_false
+    expect(user.reload.activated?).to be_false
   end
 
 end
