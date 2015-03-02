29f724d64bb2411b6ef2aa1923a12ce6df47be98
Matt Cloyd <cloydster@gmail.com>
2015-03-02 14:41:03 -0500

    Deleted Null Objects. Why were they there?

--- app/lib/active_record/add_reset_pk_sequence_to_base.rb
+++ app/lib/active_record/add_reset_pk_sequence_to_base.rb
@@ -7,9 +7,7 @@ module ActiveRecord
         new_max = maximum(primary_key) || 0
         update_seq_sql = "update sqlite_sequence set seq = #{new_max} where name = '#{table_name}';"
         ActiveRecord::Base.connection.execute(update_seq_sql)
-      when 'PostgreSQL'
-        ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
-      when 'PostGIS'
+      when 'PostgreSQL' || 'PostGIS'
         ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
       else
         raise "Task not implemented for this DB adapter, #{adapter_name}"
--- app/lib/null_objects/null_visualization.rb
+++
@@ -1,9 +0,0 @@
-class NullObjects::NullVisualization < NullObjects::NullObject
-
-  attr_accessor :sessionstate
-
-  def sessionstate
-    ""
-  end
-
-end
