ebdd9b8d2ed2332a29a818ef6f1c9dfb94ef5d52
Matt Cloyd <cloydster@gmail.com>
2015-03-11 11:40:23 -0400

    Add a bit of documentation to the institution scope.

--- app/models/concerns/institution_scope.rb
+++ app/models/concerns/institution_scope.rb
@@ -3,28 +3,31 @@ module InstitutionScope
 
   module ClassMethods
 
+    # Return all objects, with the given institution's
+    # objects first, the rest following.
+    # TODO: Would like to implement this using #sort_by{},
+    # but haven't gotten the comparison working right yet.
     def institution(institution=nil)
       institution_id = id_or_object_for(institution)
       self.unscoped.order("CASE WHEN institution_id = #{institution_id} THEN 0 ELSE 1 END")#.try(:default_scope)
     end
 
-    # TOD: This should just be institution.collaborators
-    # def only_inst(institution)
-    #   self.unscoped.where(institution_id: institution.id).default_scope
-    # end
-
-
     private
 
+      # Return the id, regardless of whether given an
+      # institution or its id.
       def id_or_object_for(institution_or_id)
         return null_institution.id if institution_or_id.nil?
         institution_or_id.try(:id) || institution_or_id
       end
 
+      # Memoize null_institution
       def null_institution
         @null_institution ||= build_null_institution
       end
 
+      # Build the null_institution as a black hole mimic
+      # with id=NULL to make the query in the scope work.
       def build_null_institution
         Naught.build { |b|
           b.black_hole
