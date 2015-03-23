dd2b4fff7d457c0131c1cf8f54e7e8a48556031c
Matt Cloyd <cloydster@gmail.com>
2015-03-23 13:30:14 -0400

    Hack Kaminari to paginate CKAN resources. Change header styles slightly.

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,5 +1,7 @@
 class Dataset
 
+  DEFAULT_PER_PAGE = 10
+
   def self.all
     CKAN::Package.find()
   end
@@ -12,23 +14,26 @@ class Dataset
   end
 
   def self.page(page_number=nil, options={})
-    # TODO: Use splat so .page(1) and .page(paginates_per: 5)
+    # TODO: Use splat so .page(1) and .page(per_page: 5)
     #       are both valid method calls.
     page_number   = (page_number || 1).to_i
-    paginates_per = options.fetch(:paginates_per) { 10 }
-
-    puts "page_number: #{page_number}"
-    puts "paginates_per: #{paginates_per}"
+    per_page = options.fetch(:per_page) { DEFAULT_PER_PAGE }
 
     records = CKAN::Package.find( 
-      rows:   paginates_per,
-      start:  page_number * paginates_per
+      rows:   per_page,
+      start:  page_number * per_page
     )
-    OpenStruct.new(records: records,
+
+    OpenStruct.new(
+      records:      records,
       current_page: page_number,
-      total_pages: 100,
-      per_page: 10,
-      )
+      total_pages:  total_pages,
+      per_page:     per_page
+    )
+  end
+
+  def self.total_pages(per_page=DEFAULT_PER_PAGE)
+    @@total_pages ||= (self.all.count / per_page)
   end
   
 end
