25769b111af699da17162fcea8693024062038c4
Matt Cloyd <cloydster@gmail.com>
2015-03-25 10:51:00 -0400

    Update views to partial out topic and dataset filters.

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -2,12 +2,17 @@ class DatasetsController < ApplicationController
   before_filter :load_institution
 
   def index
-    @datasets = Dataset.page(params[:page])
+    @datasets = if params[:topic].present?
+      rows = params[:per_page] || Dataset.per_page
+      Dataset.find_by(tags: params[:topic], rows: rows)
+    else
+      Dataset.page(params[:page])
+    end
   end
 
   def show
-    @dataset = Dataset.find_by(id: params[:id]).first
     # TODO: Fix the model so we don't need #first
+    @dataset = Dataset.find_by(id: params[:id]).first
   end
 
   def download
