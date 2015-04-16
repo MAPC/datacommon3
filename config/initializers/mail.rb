ad1d424c96e7011d4bfa82c38f399ef43c12ad45
Matt Cloyd <cloydster@gmail.com>
2015-04-16 11:40:20 -0400

    Try port 25 for mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address:               "smtp.mandrillapp.com",
-  port:                  587,
+  port:                  25,  # ports 587 and 2525 are also supported with STARTTLS
   enable_starttls_auto:  true, # detects and uses STARTTLS
   user_name:             ENV.fetch('MANDRILL_USERNAME') { '' },
   password:              ENV.fetch('MANDRILL_API_KEY')  { '' },
