04031042aab93c1de46729b6119695ddb68da7dd
Matt Cloyd <cloydster@gmail.com>
2015-04-16 17:04:10 -0400

    Filter snapshot topics with no visualizations

--- app/helpers/snapshots_helper.rb
+++ app/helpers/snapshots_helper.rb
@@ -1,5 +1,11 @@
 module SnapshotsHelper
 
+  def snapshot_topics
+    IssueArea.all.reject{|t| 
+      %w(arts-culture land-use-zoning public-safety technology).include? t.slug
+    }
+  end
+
   def options_for_area(type, selected)
     geographies = Geography
                     .institution(@institution, only: true)
