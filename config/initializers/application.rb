1782de7ca55609a00de58a93f2473ed8ec3fccdc
Matt Cloyd <cloydster@gmail.com>
2015-05-05 12:06:09 -0400

    Remove add_reset_pk file, probably autoloads.

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,6 +1,4 @@
-# require 'active_record'
-# require 'active_record/add_reset_pk_sequence_to_base'
-# require 'active_record/create_or_update'
+require 'active_record/create_or_update'
 
 require 'open-uri'
 
