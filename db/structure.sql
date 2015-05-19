8540f0a9128cf01a43c92cd92054f395fd3e7429
Matt Cloyd <cloydster@gmail.com>
2015-05-19 11:27:38 -0400

    Unclear why database structure changed, but it did, slightly.

--- db/structure.sql
+++ db/structure.sql
@@ -1066,6 +1066,14 @@ ALTER TABLE ONLY snapshots_regionalunit
 
 
 --
+-- Name: snapshots_visualization__visualization_id_7697a44685099f5a_uniq; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY snapshots_visualization_source
+    ADD CONSTRAINT snapshots_visualization__visualization_id_7697a44685099f5a_uniq UNIQUE (visualization_id, datasource_id);
+
+
+--
 -- Name: snapshots_visualization_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -1296,6 +1304,20 @@ CREATE INDEX snapshots_visualization_regiontype_id ON snapshots_visualization US
 
 
 --
+-- Name: snapshots_visualization_source_datasource_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX snapshots_visualization_source_datasource_id ON snapshots_visualization_source USING btree (datasource_id);
+
+
+--
+-- Name: snapshots_visualization_source_visualization_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX snapshots_visualization_source_visualization_id ON snapshots_visualization_source USING btree (visualization_id);
+
+
+--
 -- Name: snapshots_visualization_topics_topic_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -1373,10 +1395,18 @@ CREATE INDEX weave_visualization_topics_visualization_id ON weave_visualization_
 
 
 --
+-- Name: visualization_id_refs_id_4779ee3461d3a108; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY snapshots_visualization_source
+    ADD CONSTRAINT visualization_id_refs_id_4779ee3461d3a108 FOREIGN KEY (visualization_id) REFERENCES snapshots_visualization(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO public;
+SET search_path TO datacommon,public;
 
 INSERT INTO schema_migrations (version) VALUES ('20140901170947');
 
