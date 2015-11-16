e613ed657462f1d48b039cadec9a9c25e77e6fe9
Matt Cloyd <cloydster@gmail.com>
2015-11-16 17:10:59 -0500

    Comment about approach

--- app/helpers/error_helper.rb
+++ app/helpers/error_helper.rb
@@ -1,4 +1,6 @@
 module ErrorHelper
+  # TODO Should do what Thoughbot does and always have Airbrake
+  # present, just configure it not to fire on production.
   def trigger_airbrake(error)
     if Rails.env == 'production'
       ENV["airbrake.error_id"] = notify_airbrake(error)
